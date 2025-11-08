5692395a373bc50dede70ec9309d549cdfe8e1a6
MDEWD <1539570130@qq.com>
2025-11-08 22:55:59 +0800

    feat: 后端init

---
+++ src/main/java/com/scube/scubebackend/SCubeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.scube.scubebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SCubeBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SCubeBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scube/scubebackend/SCubeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scube.scubebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SCubeBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
